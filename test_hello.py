ef29561dd7d35e23959d8cab611301b31463fe44
jyotiranjan.nayak <jyotiranjan.nayak@effem.com>
2025-08-24 16:02:32 +0100

    Added 2nd test case

--- test_hello.py
+++ test_hello.py
@@ -3,3 +3,7 @@ from hello import say_hello
 def test_say_hello():
     assert say_hello() == "Hello, World!"
 
+def test_say_hello_not_empty():
+    result = say_hello()
+    assert result != "", "say_hello() should not return an empty string"
+
